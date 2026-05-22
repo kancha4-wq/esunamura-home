7d1e2d91e1169d4a7c14aaf7e0d4bb0e185cd0c3
kancha4-wq <251350994+kancha4-wq@users.noreply.github.com>
2026-05-22 13:56:05 +0900

    Fix Erobokishin DLsite links

--- assets/js/works.js
+++ assets/js/works.js
@@ -484,6 +484,7 @@ const salesByWorkId = {
     BOOTH: "https://esunamura.booth.pm/items/8238754",
     pictSPACE: "https://pictspace.net/items/manage_detail/851348",
     FANZA: "https://www.dmm.co.jp/dc/doujin/-/detail/=/cid=d_758170/",
+    DLsite: "https://dlaf.jp/aix/dlaf/=/t/n/link/work/aid/esunamura/id/RJ01612902.html",
     PromptCom: "https://prompt-com.com/ja/p/d3d69153-c51f-41f6-8297-1a0a693c2583",
     DiGiket: "https://www.digiket.com/work/show/_data/ID=ITM0337471/AFID=esunamura/"
   },
@@ -491,7 +492,7 @@ const salesByWorkId = {
     BOOTH: "https://esunamura.booth.pm/items/8284888",
     pictSPACE: "https://pictspace.net/items/manage_detail/858788",
     FANZA: "https://www.dmm.co.jp/dc/doujin/-/detail/=/cid=d_763759/",
-    DLsite: "https://dlaf.jp/aix/dlaf/=/t/n/link/work/aid/esunamura/id/RJ01612902.html",
+    DLsite: "https://dlaf.jp/aix/dlaf/=/t/n/link/work/aid/esunamura/id/RJ01622810.html",
     PromptCom: "https://prompt-com.com/ja/p/cdd31e47-ba82-4793-939d-8445864efceb",
     DiGiket: "https://www.digiket.com/work/show/_data/ID=ITM0337472/AFID=esunamura/"
   },
